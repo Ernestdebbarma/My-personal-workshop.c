6adecc0a0cdd75a3b1e81d2cedf3d023f79cc176
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-06 18:47:52 +0530

    while reference| Do-while reference
    This helps understand real quick about while.

---
+++ do-while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+   int o,k, p =1 ;
+   printf("what the hell is going on.");
+   scanf("%d",&o);
+   do
+   {
+      printf("              %d\n",k=o*9);
+      o++;
+   }
+   while(k<=100);
+
+   return 0;
+}
