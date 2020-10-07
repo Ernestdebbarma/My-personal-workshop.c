52c0d49147737067cfe563128f07bc327ebacda8
Ernest Debbarma <72308267+Ernestdebbarma@users.noreply.github.com>
2020-10-07 20:01:41 +0530

    ascii values in forming words....yess....

--- printing words.c
+++ printing words.c
@@ -5,3 +5,25 @@ int main() {
         printf("%c ", c);
     return 0;
 }
+
+/*
+#include<stdio.h>
+int main()
+{
+   printf("%d",'Z');
+
+    return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i=65;
+
+    for(i=65;i<=90;i++)
+    {
+        printf("%c and its ascii value is %d\n",i,i);
+    }
+}
+
